56a5407ec107983989ece0e03026b7abfe713086
Valdix <novaldix@gmail.com>
2020-12-08 00:52:37 +0300

    Переносит и рефакторит все компоненты из Main в presenter/Trip

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,8 +1,10 @@
 import {IS_NEW_MODE, WarningTypes} from "../utils/constants";
+import {render, replace} from "../utils/render";
 import {getBlankPoint} from "../temp/mocks";
 
 import {
   EventEditView,
+  EventView,
   SortView,
   TimetableView,
   TripView,
@@ -22,26 +24,84 @@ export default class Trip {
 
   init(points) {
     this._points = points.slice();
+
+    render(this._tripContainer, this._tripComponent);
+
+    this._renderTrip();
   }
 
   _renderSort() {
-    // Метод для рендеринга сортировки
+    render(this._tripComponent, this._sortComponent);
   }
 
-  _renderEvent() {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов эвента/эдит-эвента,
-    // текущая функция renderTask в main.js
+  _renderEvent(point) {
+    const eventComponent = new EventView(point);
+    const eventEditComponent = new EventEditView(point);
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        setViewMode();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    const setEditMode = () => replace(eventEditComponent, eventComponent);
+
+    const setViewMode = () => replace(eventComponent, eventEditComponent);
+
+    const formSubmitDummy = ({type, submitter}) => {
+      throw new Error(`Need to implement a handler ${type} in "${submitter.className}"`);
+    };
+
+    eventComponent.rollupButtonClick(() => {
+      setEditMode();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventEditComponent.rollupButtonClick(() => {
+      setViewMode();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventEditComponent.resetButtonClick(() => {
+      setViewMode();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventEditComponent.formSubmit((evt) => {
+      formSubmitDummy(evt);
+    });
+
+    render(this._timetableElement, eventComponent);
   }
 
   _renderEvents() {
-    // Метод для рендеринга всего списка эвентов за раз
+    this._points.forEach((point) => this._renderEvent(point));
   }
 
-  _renderEmptyEvents() {
-    // Метод для рендеринга заглушки
+  _renderNoEvents() {
+    render(this._tripComponent, this._warningComponent);
+  }
+
+  _renderNewEvent() {
+    render(this._timetableComponent, this._blankComponent);
+  }
+
+  _renderTimetable() {
+    render(this._tripComponent, this._timetableComponent);
+    this._renderNewEvent();
+
+    this._renderEvents();
   }
 
   _renderTrip() {
-    // Метод для инициализации (начала работы) модуля,
+    if (this._points.length === 0) {
+      this._renderNoEvents();
+      return;
+    }
+
+    this._renderSort();
+    this._renderTimetable();
   }
 }
