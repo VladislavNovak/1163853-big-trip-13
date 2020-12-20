c9c847b7fdff4535e894e6799d732ecba6092fc8
Valdix <novaldix@gmail.com>
2020-12-20 14:07:31 +0300

    Устанавливает пакет he для экранирования ввода в destination input

--- src/view/event-edit/templates/create-header-destination-template.js
+++ src/view/event-edit/templates/create-header-destination-template.js
@@ -1,4 +1,5 @@
 import {getPlaces} from "../../../utils";
+import he from 'he';
 
 export const createHeaderDestinationTemplate = ({type, place: selectedPlace}) => {
 
@@ -16,7 +17,14 @@ export const createHeaderDestinationTemplate = ({type, place: selectedPlace}) =>
       <label class="event__label  event__type-output" for="event-destination-1">
         ${type}
       </label>
-      <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${selectedPlace}" list="destination-list-1">
+      <input
+        class="event__input
+        event__input--destination"
+        id="event-destination-1"
+        type="text"
+        name="event-destination"
+        value="${he.encode(selectedPlace)}"
+      list="destination-list-1">
       <datalist id="destination-list-1">
         ${getOption()}
       </datalist>
