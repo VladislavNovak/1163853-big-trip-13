9ab65619a89bfd2fe5f2f9b0a5a1e415a4d10e16
Valdix <novaldix@gmail.com>
2021-01-08 18:13:03 +0300

    Подписывает presenter/Info._handleModelEvent к обзёрверу

--- src/presenter/info.js
+++ src/presenter/info.js
@@ -1,4 +1,5 @@
-import {render, RenderPosition} from "../utils/render";
+import {batchBind} from "../utils";
+import {remove, render, RenderPosition} from "../utils/render";
 import {InfoView} from "../view";
 
 export default class Info {
@@ -6,10 +7,35 @@ export default class Info {
     this._headerContainer = headerContainer;
     this._eventsModel = eventsModel;
     this._infoComponent = null;
+
+    batchBind(this, this._handleModelEvent);
+    this._eventsModel.addObserver(this._handleModelEvent);
   }
 
   init() {
+    this._renderInfoComponent();
+  }
+
+  _renderInfoComponent() {
     this._infoComponent = new InfoView(this._eventsModel);
     render(this._headerContainer, this._infoComponent, RenderPosition.AFTERBEGIN);
   }
+
+  _destroyInfoComponent() {
+    if (this._infoComponent === null) {
+      return;
+    }
+
+    remove(this._infoComponent);
+    this._infoComponent = null;
+  }
+
+  _handleModelEvent() {
+    console.log(`FIRE`);
+
+    this._destroyInfoComponent();
+    this._renderInfoComponent();
+  }
 }
+
+// _handleModelEvent: обработчик-наблюдатель, который реагирует на изменение модели.
